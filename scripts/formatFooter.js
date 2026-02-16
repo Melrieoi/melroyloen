2c0f59ab8b1447f7f6383417ffaf564b3b973ced
Melrieoi <melroy.tygo.loen@gmail.com>
2026-02-16 14:38:25 +0100

    created footer

--- scripts/formatFooter.js
+++ scripts/formatFooter.js
@@ -1,13 +1,15 @@
-let footer = document.getElementsByTagName("footer");
+let footer = document.getElementById("footer");
 
 let naam = document.createElement("p");
 let contactMail = document.createElement("p");
 let contactPhone = document.createElement("p");
 
-naam.setAttribute("id", "footerPname");
+naam.setAttribute("id", "footerPcontact");
 contactMail.setAttribute("id", "footerPcontact");
 contactPhone.setAttribute("id", "footerPcontact");
 
-naam.innerHTML = `Melroy Loen`;
+naam.innerHTML = `<p>Naam: Melroy Loen</p>`;
+contactMail.innerHTML = `<p>E-Mail: melroy.tygo.loen@gmail.com</p>`;
+contactPhone.innerHTML = `<p>Telefoonnummer: 0642849604</p>`;
 
-footer.appendChild(naam, contactMail, contactPhone);
+footer.append(naam, contactMail, contactPhone);
--- scripts/formatNav.js
+++ scripts/formatNav.js
@@ -1,7 +1,7 @@
-<li><a href="#">Opleidingen</a>
+{/* <li><a href="#">Opleidingen</a>
     <ul>
         <li><a href="#">VMBO</a></li>
         <li><a href="#">MBO</a></li>
         <li><a href="#">HBO</a></li>
     </ul>
-</li>
+</li> */}
